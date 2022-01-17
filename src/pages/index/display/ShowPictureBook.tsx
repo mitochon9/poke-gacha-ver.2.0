96c9d7e962a209899c7b2ff3869b34b4926af150
mitochon9 <mitochon9@gmail.com>
2022-01-17 19:35:16 +0900

    🎨 データをcookie から localStorage へ変更

--- src/pages/index/display/ShowPictureBook.tsx
+++ src/pages/index/display/ShowPictureBook.tsx
@@ -1,20 +1,22 @@
 import Image from "next/image";
-import { useCookies } from "react-cookie";
 import type { PokemonData } from "src/type/pokemonData";
 
 export const ShowPictureBook = () => {
-  const [cookies] = useCookies();
-  const pokemonData: PokemonData = cookies.pokemonData;
+  const storageData = localStorage.getItem("storageData");
+
+  const parsedStorageData: PokemonData = storageData ? JSON.parse(storageData) : [];
 
   return (
     <div className="overflow-auto w-auto h-60 md:h-80">
-      <h1>つかまえたポケモン</h1>
-      <div className="flex justify-center">
-        <div className="space-y-2">
-          {pokemonData.map((data, index) => (
-            <div key={index} className="grid items-end w-72 border-2 border-gray-500">
+      <h1 className="text-xs">つかまえたポケモン</h1>
+      <div className="px-2 space-y-2 w-auto h-52">
+        {parsedStorageData?.length === 0 ? (
+          <div></div>
+        ) : (
+          parsedStorageData?.map((data, index) => (
+            <div key={index} className="grid items-end text-xs rounded border-2 border-gray-500">
               <div className="col-span-3">
-                <Image src={data.img} alt={data.name} width={120} height={120} />
+                <Image src={data.img} alt={data.name} width={80} height={80} />
                 <div>No.{data.id}</div>
               </div>
               <div className="col-span-4 leading-loose text-left">
@@ -23,10 +25,10 @@ export const ShowPictureBook = () => {
                 <div>{(data.height / 10).toFixed(1)} m</div>
                 <div>{(data.weight / 10).toFixed(1)} kg</div>
               </div>
-              <div className="col-span-7 border-t-2 border-gray-500">{data.flavorText}</div>
+              <div className="col-span-7 p-2 h-24 text-left border-t-2 border-gray-500">{data.flavorText}</div>
             </div>
-          ))}
-        </div>
+          ))
+        )}
       </div>
     </div>
   );
--- src/pages/index/display/TopDisplay.tsx
+++ src/pages/index/display/TopDisplay.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-handler-names */
 import Image from "next/image";
 
 export const TopDisplay = () => (
@@ -5,17 +6,24 @@ export const TopDisplay = () => (
     <h1 className="text-2xl font-extrabold text-center scale-y-125">ポケットモンスター</h1>
     <h2 className="mt-1 text-sm font-bold tracking-widest text-center">POCKET MONSTERS</h2>
     <h3 className="mt-1 text-xs font-bold text-center">Red Version</h3>
-    <p className="text-xs flash">Push start or A button</p>
+    <p className="text-xs flash">Press start or A button</p>
+    <p className="text-[8px]">セレクトボタンでモンスター図鑑へ</p>
     <div className="flex justify-center items-end md:mt-8">
-      <div className="-mr-5">
-        <Image src="/red.png" alt="メインビジュアル" width={120} height={120} />
+      <div className="-mr-4">
+        <Image src="/red.png" alt="メインビジュアル" width={100} height={100} />
       </div>
-      <div className="-mb-2 -ml-5">
-        <Image src="/hitokage.png" alt="メインビジュアル" width={100} height={100} />
+      <div className="-mb-2 -ml-4">
+        <Image src="/hitokage.png" alt="メインビジュアル" width={80} height={80} />
       </div>
     </div>
     <small className="absolute bottom-0 left-1/2 w-full text-[8px] text-center -translate-x-1/2">
       ©1995 GAME FREAK Inc.
     </small>
+    <button
+      onClick={() => localStorage.clear()}
+      className="absolute right-0 bottom-0 px-1 text-[8px] rounded border border-gray-700"
+    >
+      データ削除
+    </button>
   </div>
 );
