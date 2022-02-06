24457fa18ec80350576a2e583dd53631ed104792
mitochon9 <mitochon9@gmail.com>
2022-02-06 21:55:20 +0900

    ♻️ refactor create volume hole

--- src/pages/index/Speaker.tsx
+++ src/pages/index/Speaker.tsx
@@ -1,91 +1,40 @@
-export const Speaker = () => (
-  <div className="absolute right-3 -bottom-2 -rotate-12">
-    <div className="grid grid-cols-8 grid-rows-[8] gap-[2px] w-20 h-20">
-      {volumeHole.map((item, index) => (
-        <div
-          key={index}
-          className={`w-1 h-1 rounded-full bg-gray-600 ${
-            index === 0
-              ? "col-start-2"
-              : index === 5
-              ? "col-span-2"
-              : index === 54
-              ? "col-start-2"
-              : index === 59
-              ? "col-span-2"
-              : "col-span-1"
-          }`}
-        >
-          {item}
-        </div>
-      ))}
-    </div>
-  </div>
-);
+import { useCallback, useEffect, useState } from "react";
 
-const volumeHole = [
-  // "",
-  // "",
-  // "",
-  // "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-];
+export const Speaker = () => {
+  const [volumeHole, setVolumeHole] = useState<string[]>([]);
+
+  const createVolumeHole = useCallback(() => {
+    for (let index = 0; index < 60; index++) {
+      setVolumeHole((volumeHole) => [...volumeHole, ""]);
+    }
+  }, []);
+
+  useEffect(() => {
+    createVolumeHole();
+  }, [createVolumeHole]);
+
+  return (
+    <div className="absolute right-3 -bottom-2 -rotate-12">
+      <div className="grid grid-cols-8 grid-rows-[8] gap-[2px] w-20 h-20">
+        {volumeHole.map((item, index) => (
+          <div
+            key={index}
+            className={`w-1 h-1 rounded-full bg-gray-600 ${
+              index === 0
+                ? "col-start-2"
+                : index === 5
+                ? "col-span-2"
+                : index === 54
+                ? "col-start-2"
+                : index === 59
+                ? "col-span-2"
+                : "col-span-1"
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
