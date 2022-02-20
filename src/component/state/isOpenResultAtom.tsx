4733c47c05733a1381245292192125e99a40c79d
mitochon9 <mitochon9@gmail.com>
2022-02-21 02:00:21 +0900

    🚚 rename files

--- src/component/state/isOpenResultAtom.tsx
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const isOpenResultState = atom<boolean>({
-  key: "isOpenResultState",
-  default: false,
-});
--- src/component/state/isShowPictureBookAtom.tsx
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const isShowPictureBookState = atom<boolean>({
-  key: "isShowPictureBookState",
-  default: false,
-});
