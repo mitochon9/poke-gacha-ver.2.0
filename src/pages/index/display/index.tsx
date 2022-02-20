4baad8b9028d20cc6175f5c007bf17d67560a692
mitochon9 <mitochon9@gmail.com>
2022-02-21 02:05:00 +0900

    ♻️ refactor top display

--- src/pages/index/display/index.tsx
+++ src/pages/index/display/index.tsx
@@ -1,29 +1,37 @@
-import Image from "next/image";
 import { useRecoilValue } from "recoil";
 import { isAnimationState } from "src/component/state/isAnimationAtom";
-import { isOpenResultState } from "src/component/state/isOpenResultAtom";
-import { isShowPictureBookState } from "src/component/state/isShowPictureBookAtom";
-import { ShowPictureBook } from "src/pages/index/display/ShowPictureBook";
+import { isDeletionCompletedState } from "src/component/state/isDeletionCompletedAtom";
+import { isDeletionConfirmationState } from "src/component/state/isDeletionConfirmationAtom";
+import { isPokedexState } from "src/component/state/isPokedexAtom";
+import { isResultDisplayState } from "src/component/state/isResultDisplayAtom";
+import { DeletionCompleted } from "src/pages/index/display/DeletionCompleted";
+import { DeletionConfirmation } from "src/pages/index/display/DeletionConfirmation";
+import { MonsterBallAnimation } from "src/pages/index/display/MonsterBallAnimation";
+import { Pokedex } from "src/pages/index/display/Pokedex";
+import { ResultDisplay } from "src/pages/index/display/ResultDisplay";
 import { TopDisplay } from "src/pages/index/display/TopDisplay";
-import { ShowPokemon } from "src/pages/index/ShowPokemon";
 
 export const Display = () => {
-  const isOpenResult = useRecoilValue(isOpenResultState);
+  const isResultDisplay = useRecoilValue(isResultDisplayState);
   const isAnimation = useRecoilValue(isAnimationState);
-  const isShowPictureBook = useRecoilValue(isShowPictureBookState);
+  const isPokedex = useRecoilValue(isPokedexState);
+  const isDeletionConfirmation = useRecoilValue(isDeletionConfirmationState);
+  const isDeletionCompleted = useRecoilValue(isDeletionCompletedState);
 
   return (
     <div className="relative mx-2 mt-4 font-dot text-center bg-amber-50 rounded-2xl border-x-[56px] border-t-[36px] border-b-[56px] border-gray-700 md:mx-8 md:mt-8">
       {isAnimation ? (
-        <div className="flex justify-center items-center w-auto h-60 md:h-80">
-          <Image src="/monsterBall.png" alt="モンスターボール" width={60} height={60} className="ball-animation" />
-        </div>
-      ) : isShowPictureBook ? (
-        <ShowPictureBook />
-      ) : !isOpenResult ? (
-        <TopDisplay />
+        <MonsterBallAnimation />
+      ) : isPokedex ? (
+        <Pokedex />
+      ) : isResultDisplay ? (
+        <ResultDisplay />
+      ) : isDeletionConfirmation ? (
+        <DeletionConfirmation />
+      ) : isDeletionCompleted ? (
+        <DeletionCompleted />
       ) : (
-        <ShowPokemon />
+        <TopDisplay />
       )}
     </div>
   );
