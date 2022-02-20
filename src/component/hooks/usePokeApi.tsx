f06f0d6e41febfbe4e3033b8cc254b80642f5b9b
mitochon9 <mitochon9@gmail.com>
2022-02-21 02:02:51 +0900

    🎨 format of the code

--- src/component/hooks/usePokeApi.tsx
+++ src/component/hooks/usePokeApi.tsx
@@ -1,7 +1,7 @@
 import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
 
-export const usePokeApi01 = (pokemonId: number | undefined) => {
+export const usePokeApi = (pokemonId: number | undefined) => {
   const { data, error } = useSWR(
     `https://pokeapi.co/api/v2/pokemon/
 ${pokemonId}`,
@@ -18,7 +18,7 @@ ${pokemonId}`,
   };
 };
 
-export const usePokeApi02 = (pokemonId: number | undefined) => {
+export const useJapanesePokeApi = (pokemonId: number | undefined) => {
   const { data, error } = useSWR(
     `https://pokeapi.co/api/v2/pokemon-species/
 ${pokemonId}`,
