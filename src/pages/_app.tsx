5212cdcbc7bf6ac1ef4f77ddbbdc1ac14645c307
mitochon9 <mitochon9@gmail.com>
2022-06-11 18:51:56 +0900

    feat: Recoil のセットアップ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import "src/style/globals.css";
 
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <RecoilRoot>
+    <Component {...pageProps} />
+  </RecoilRoot>
 );
 
 export default App;
