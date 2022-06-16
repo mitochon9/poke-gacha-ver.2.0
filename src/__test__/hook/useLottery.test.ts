23d2e6b5e50d64f832cc4bfb7519ce5b6b84522f
mitochon9 <mitochon9@gmail.com>
2022-06-16 23:34:10 +0900

    fix: winningNumber の初期値修正

--- src/__test__/hook/useLottery.test.ts
+++ src/__test__/hook/useLottery.test.ts
@@ -3,11 +3,12 @@ import { RecoilRoot } from "recoil";
 import { useLottery } from "src/hook/useLottery";
 
 describe("useLottery", () => {
-  it("最初に Hook を呼び出した時、 初期値に が１が入っていること", () => {
+  it("最初に Hook を呼び出した時、 初期値に が１〜１５１の番号 winningNumber にセットされていること", () => {
     const { result } = renderHook(() => useLottery(), {
       wrapper: RecoilRoot,
     });
-    expect(result.current.winningNumber).toEqual(1);
+    expect(result.current.winningNumber).toBeGreaterThanOrEqual(1);
+    expect(result.current.winningNumber).toBeLessThanOrEqual(151);
   });
 
   it("抽選をした際に１〜１５１の番号が winningNumber にセットされていること", () => {
